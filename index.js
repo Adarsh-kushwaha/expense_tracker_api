393bb04561ec1e9818a3814ed5d2ac1f73eae623
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-04-16 09:59:26 +0530

    added dynamic port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-port = 5000;
+port = process.env.PORT || 5000;
 const transacation = require("./routes/transaction")
 const connectDB = require('./config/db');
 
